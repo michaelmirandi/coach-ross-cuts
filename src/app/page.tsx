8546aaf5ab4a6b06d92ed0a9d94492b8703c19a2
Michael Mirandi <michaelmirandi@Michaels-MacBook-Pro-2.local>
2026-05-04 16:29:24 -0400

    :update images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,10 +17,10 @@ export default function Home() {
   
   // Images from public directory, ordered 1-6
   const imageUrls = [
-    "/1.jpg",
+    "/7.jpg",
     "/2.jpg", 
-    "/3.jpg",
-    "/4.jpg",
+    "/8.jpg",
+    "/9.jpg",
     // "/5.jpg",
     "/6.jpg",
   ];
